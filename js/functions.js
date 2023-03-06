24d06ed11b4f8e8f5773225b4e4aea9b3cb1b7fd
NovikovVadim <vadim_novikov@outlook.com>
2023-03-06 20:14:13 +0300

    module2

--- js/functions.js
+++ js/functions.js
@@ -1,24 +1,24 @@
 //Функция для проверки длины строки
 
 const checkString = (string, length) => string.length <= length;
-
+checkString('', 1);
 //Функция для проверки, является ли строка палиндромом
 
 const isPalindrom = (string) => {
-  const tempString = string.toLowerCase().replaceAll(" ", "");
+  const tempString = string.toLowerCase().replaceAll(' ', '');
 
-  let reverseString = "";
+  let reverseString = ' ';
   for (let i = tempString.length - 1; i >= 0; i--) {
     reverseString += tempString.at(i);
   }
 
   return tempString === reverseString;
 };
-
+isPalindrom('');
 //Функция, которая принимает строку, извлекает содержащиеся в ней цифры от 0 до 9 и возвращает их в виде целого положительного числа
 
 const extractNumber = (string) => {
-  let result = "";
+  let result = '';
 
   for (let i = 0; i < string.length; i++) {
     if (!Number.isNaN(parseInt(string.at(i), 10))) {
@@ -28,6 +28,7 @@ const extractNumber = (string) => {
 
   return parseInt(result, 10);
 };
+extractNumber('');
 
 //Функция, которая принимает три параметра: исходную строку, минимальную длину и строку с добавочными символами — и возвращает исходную строку, дополненную указанными символами до заданной длины.
 
@@ -39,6 +40,8 @@ const padStart = (string, minLength, pad) => {
       newResultLength <= minLength
         ? pad
         : pad.slice(0, minLength - newResultLength);
+    actualPad();
   }
   return result;
 };
+padStart('', 1, '');
